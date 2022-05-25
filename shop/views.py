2aeb3b1b3720ad92b66e2c2eb931385976a152db
Kushagra Pandey <kushagrajnvgkp@gmail.com>
2022-05-25 23:33:47 +0530

    GET

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,21 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.template import loader
+from django.core import serializers
+from . models import Shop
+"""
+requests.method => GET,POST,UPDATE if 
+get => requsts.GET 
+post => requersts.POST
 
-# Create your views here.
-def index(res):
-    return HttpResponse("Helloworld")
+"""
+def index(request):
+  allitems = Shop.objects.all().values()
+  output = ""
+  for x in allitems:
+    output += x["name"]
+  return HttpResponse(output)
+
+def shop(requests):
+      wholedata= Shop.objects.all()
+      serilize = serializers.serialize('json', wholedata)
+      return JsonResponse(serilize, safe= False)
--- supermarket/urls.py
+++ supermarket/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
+
 from django.urls import path
-from shop.views import index
+from shop.views import index,shop
 urlpatterns = [
     path("",index),
-    path('admin/', admin.site.urls),
+    path("shop",shop),
 ]
